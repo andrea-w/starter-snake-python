fb4a01b387765a9c55fae2d52dbabcf0e529c832
Shea Faigan <stfaigan@gmail.com>
2019-03-02 15:07:08 -0800

    Circles forever

--- app/main.py
+++ app/main.py
@@ -2,8 +2,30 @@ import json
 import os
 import random
 import bottle
+from a_star import *
 
 from api import ping_response, start_response, move_response, end_response
+
+def checkForObstacle(data, x, y):
+
+    walls = {
+        'up': 0,
+        'right': data['board']['width']-1,
+        'down': data['board']['height']-1,
+        'left': 0
+    }
+
+    for snake in data['board']['snakes']:
+        for piece in snake['body']:
+            if piece['x'] == x and piece['y'] == y:
+                print("snake found at ", x, y)
+                return True
+
+    if walls["up"] == y or walls["down"] == y or walls["left"] == x or walls["right"] == x:
+        print("wall found at ", x, y)
+        return True
+
+    return False
 
 @bottle.route('/')
 def index():
@@ -26,7 +48,7 @@ def static(path):
 def ping():
     """
     A keep-alive endpoint used to prevent cloud application platforms,
-    such as Heroku, from sleeping the application instance.
+    such as Heroku,  from sleeping the application instance.
     """
     return ping_response()
 
@@ -39,9 +61,14 @@ def start():
             initialize your snake state here using the
             request's data if necessary.
     """
-    print(json.dumps(data))
+
+    food_initial = data["board"]["food"]
+
+    print(json.dumps(data, indent=4))
 
     color = "#00FF00"
+    headType: "beluga"
+    tailType: "pixel"
 
     return start_response(color)
 
@@ -50,14 +77,70 @@ def start():
 def move():
     data = bottle.request.json
 
+    head_pos = {
+        'x': data['you']['body'][0]['x'], 
+        'y': data['you']['body'][0]['y']
+    }
+
+    # walls = {
+    #     'up': 0,
+    #     'right': data['board']['width']-1,
+    #     'down': data['board']['height']-1,
+    #     'left': 0
+    # }
+
+    # wallFlag = {
+    #     'up': head_pos['y'] <= walls['up'],
+    #     'right': head_pos['x'] >= walls['right'],
+    #     'down': head_pos['y'] >= walls['down'],
+    #     'left': head_pos['x'] <= walls['left']
+    # }
+
+    obstacleFlag = {
+        'up': checkForObstacle(data, head_pos['x'], head_pos['y'] - 1),
+        'right': checkForObstacle(data, head_pos['x'] + 1, head_pos['y']),
+        'down': checkForObstacle(data, head_pos['x'], head_pos['y'] + 1),
+        'left': checkForObstacle(data, head_pos['x'] - 1, head_pos['y'])
+    }
+    direction = 'right'
+
+    # if all(value == False for value in wallFlag.values()):
+    #     direction = "right"
+    #     print("NOT NEAR ANY WALLS")
+
+    # if wallFlag['up'] or wallFlag['down']:
+    #     if wallFlag['right'] and not bodyFlag['left']:
+    #         direction = 'left'
+    #     if wallFlag['left'] and not bodyFlag['right']:
+    #         direction = 'right'
+
+    # elif wallFlag['right'] or wallFlag['left']:
+    #     direction = "up"
+    #     if wallFlag['up'] and not bodyFlag['down']:
+    #         direction = 'down'
+    #     if wallFlag['down']and not bodyFlag['up']:
+    #         direction = 'up'
+
+    if not obstacleFlag['up']:
+        direction = 'up'
+    if not obstacleFlag['right']:
+        direction = 'right'
+    if not obstacleFlag['left']:
+        direction = 'left'
+    if not obstacleFlag['down']:
+        direction = 'down'
+
     """
     TODO: Using the data from the endpoint request object, your
             snake AI must choose a direction to move in.
     """
-    print(json.dumps(data))
+    print(json.dumps(data, indent=4))
 
     directions = ['up', 'down', 'left', 'right']
-    direction = random.choice(directions)
+    #direction = random.choice(directions)
+
+    # print("WALL FLAG = " + str(wallFlag))
+    # print("DIRECTION = " + direction)
 
     return move_response(direction)
 
@@ -70,7 +153,7 @@ def end():
     TODO: If your snake AI was stateful,
         clean up any stateful objects here.
     """
-    print(json.dumps(data))
+    print(json.dumps(data, indent=4))
 
     return end_response()
 
