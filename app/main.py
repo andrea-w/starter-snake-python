5207f0718134d9ac50be7b9117f3e994fc25627d
Shea Faigan <stfaigan@gmail.com>
2019-03-02 16:05:07 -0800

    it's fixed

--- app/main.py
+++ app/main.py
@@ -233,6 +233,6 @@ if __name__ == '__main__':
     bottle.run(
         application,
         host=os.getenv('IP', '0.0.0.0'),
-        port=os.getenv('PORT', '18080'),
+        port=os.getenv('PORT', '8080'),
         debug=os.getenv('DEBUG', True)
     )
