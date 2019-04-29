b09bb309b53ee2aa5251435caad61c60b5068145
Andrea <andrea.k.williams7@gmail.com>
2019-04-28 23:00:30 -0700

    Can avoid own body and other snakes, find adjacent food. HOC & self-trapping common

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ import json
 import os
 import random
 import bottle
+import scipy
 
 from api import ping_response, start_response, move_response, end_response
 
@@ -41,7 +42,7 @@ def start():
     """
     print(json.dumps(data))
 
-    color = "#00FF00"
+    color = "#00FF99"
 
     return start_response(color)
 
@@ -57,9 +58,25 @@ def move():
     print(json.dumps(data))
 
     directions = ['up', 'down', 'left', 'right']
-    direction = random.choice(directions)
+    #direction = random.choice(directions)
+    direction = 'right'
 
-    return move_response(direction)
+    head_pos = get_position_of_my_head(data)
+    adj_points = get_all_4_points(head_pos)
+    no_walls = check_for_walls(adj_points, data)
+    avoid_me = check_own_body(no_walls, data)
+    avoid_others = check_for_other_snakes(avoid_me, data)
+
+    next_to_food = check_if_next_to_food(avoid_others, data)
+    if (next_to_food != False):
+        del avoid_others[:]
+        avoid_others.append(next_to_food)
+
+    possible_directions = []
+    for point in avoid_others:
+        possible_directions.append(get_name_of_direction(point,head_pos))
+
+    return move_response(random.choice(possible_directions))
 
 
 @bottle.post('/end')
@@ -74,6 +91,92 @@ def end():
 
     return end_response()
 
+def get_position_of_my_head(data):
+    return data['you']['body'][0]
+
+def get_point_to_left(pos):
+    return {"x": int(pos.get("x")) - 1, "y": int(pos.get("y"))}
+
+def get_point_to_right(pos):
+    return {"x": int(pos.get("x")) + 1, "y": int(pos.get("y"))}
+
+def get_point_upward(pos):
+    return {"x": int(pos.get("x")), "y": int(pos.get("y")) - 1}
+
+def get_point_downward(pos):
+    return {"x": int(pos.get("x")), "y": int(pos.get("y")) + 1}
+
+def get_all_4_points(pos):
+    coords = []
+    coords.append(get_point_to_left(pos))
+    coords.append(get_point_to_right(pos))
+    coords.append(get_point_upward(pos))
+    coords.append(get_point_downward(pos))
+    return coords
+
+# coords is list (len 4) of (x,y) coordinates of 4 possible points to move to
+def check_for_walls(coords, data):
+    ok_coords = []
+    # check left
+    if int(coords[0].get('x')) > -1:
+        ok_coords.append(coords[0])
+    # check right
+    if int(coords[1].get('x')) < data['board']['width']:
+        ok_coords.append(coords[1])
+    # check up
+    if int(coords[2].get('y')) > -1:
+        ok_coords.append(coords[2])
+    # check down
+    if int(coords[3].get('y')) < data['board']['height']:
+        ok_coords.append(coords[3])
+
+    return ok_coords
+
+def check_own_body(coords, data):
+    ok_coords = coords
+    for point in coords:
+        if point in data['you']['body']:
+            ok_coords.remove(point)
+            print("avoided collision with self at " + str(point))
+
+    return ok_coords
+
+def check_for_other_snakes(coords, data):
+    ok_coords = coords
+    for point in coords:
+        for snake in data['board']['snakes']:
+            if point in snake['body']:
+                ok_coords.remove(point)
+                print("avoided collision with another snake at " + str(point))
+    return ok_coords
+
+def check_if_next_to_food(coords, data):
+    for food in data['board']['food']:
+        if food in coords:
+            return food
+    return False
+
+def look_for_food(head_pos, data):
+    min_dist = 99
+    nearest_food_loc = {}
+    for food in data['board']['food']:
+        dist = scipy.spatial.distance.cityblock(food, head_pos)
+        if (dist < min_dist):
+            min_dist = dist
+            nearest_food_loc = food
+    return nearest_food_loc
+
+def get_name_of_direction(dest, src):
+    if (int(dest.get('x')) < int(src.get('x'))):
+        return 'left'
+    elif (int(dest.get('x')) > int(src.get('x'))):
+        return 'right'
+    elif (int(dest.get('y') < src.get('y'))):
+        return 'up'
+    else:
+        return 'down'
+
+
 # Expose WSGI app (so gunicorn can find it)
 application = bottle.default_app()
 
@@ -81,6 +184,6 @@ if __name__ == '__main__':
     bottle.run(
         application,
         host=os.getenv('IP', '0.0.0.0'),
-        port=os.getenv('PORT', '3011'),
+        port=os.getenv('PORT', '3200'),
         debug=os.getenv('DEBUG', True)
     )
